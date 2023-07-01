e1e769eb33a680571cbc2cb0d41825514383385c
sergio15dev <sergio15dev@gmail.com>
2023-07-01 11:09:13 -0500

    Complete Potencia de un numero

---
+++ 3_Tarea_Divide_Y_Venceras/5_Potencia_Numero.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+double Potencia(double base, double exponente){ // 2^10
+    if(exponente > 0){
+        return base * Potencia(base,exponente-1); // 2 * 2^9 ... 2 * 2^8
+    } else if(exponente < 0) { // caso de exponente negativo
+        return 1.0 / (base * Potencia(base,-(exponente) - 1));
+    }
+    return 1.0;
+}
+
+int main(){
+    double num = 5;
+    double exponente = -2;
+    double resultado = Potencia(num,exponente);
+    cout<<"Resultado => "<<resultado*5;
+    return 0;
+}
