a657cba4a0bfd4f652a2bc9b40f7bbaa4cf022fe
sergio15dev <sergio15dev@gmail.com>
2023-07-01 07:59:19 -0500

    Complete MergeSort

--- 3_Tarea_Divide_Y_Venceras/2_MergeSort.cpp
+++ 3_Tarea_Divide_Y_Venceras/2_MergeSort.cpp
@@ -62,8 +62,9 @@ void printVector(int Array[], int size)
 
 int main(){
     int array1[] = {3, 2, 5, 0, 1, 8, 7, 6, 9, 4};
-    printVector(array1, 10);
-    MyMergeSort(array1, 0, 9);
-    printVector(array1, 10);
+    int array2[] = {23, 5, 17, 28, 1, 11, 30, 6, 19, 8, 25, 13, 4, 16, 2, 7, 20, 9, 12, 22, 15, 10, 3, 27, 18, 14, 24, 26, 29, 21};
+    printVector(array2, 10);
+    MyMergeSort(array2, 0, 9);
+    printVector(array2, 10);
     return 0;
 }
