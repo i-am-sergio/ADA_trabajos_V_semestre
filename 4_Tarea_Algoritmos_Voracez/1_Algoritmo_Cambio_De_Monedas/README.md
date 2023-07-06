3e20471ee22cbcd1916e32fe3d724511085fe599
sergio15dev <sergio15dev@gmail.com>
2023-07-05 21:23:50 -0500

    screenshots

---
+++ 4_Tarea_Algoritmos_Voracez/1_Algoritmo_Cambio_De_Monedas/README.md
@@ -0,0 +1,5 @@
+# Problema del Cambio de Monedas
+
+## Ejecucion
+
+![Ruta de la imagen](ejecucion.png)
--- 4_Tarea_Algoritmos_Voracez/1_Algoritmo_Cambio_De_Monedas/monedas.cpp
+++ 4_Tarea_Algoritmos_Voracez/1_Algoritmo_Cambio_De_Monedas/monedas.cpp
@@ -4,25 +4,29 @@
 #include <algorithm>
 using namespace std;
 
-
-void devolverCambio(int P, vector<int>& C, vector<int>& X){
+void devolverCambio(int P, vector<int> &C, vector<int> &X)
+{
     sort(C.begin(), C.end());
     int n = C.size();
     int actual = 0;
-    int j = n-1;
-    for (int i=0; i<n; i++){ // se inicializa con ceros
+    int j = n - 1;
+    for (int i = 0; i < n; i++)
+    { // se inicializa con ceros
         X[i] = 0;
     }
-    while(actual != P){
-        while((C[j] > (P-actual)) && (j>0)){
-            j = j-1;
+    while (actual != P)
+    {
+        while ((C[j] > (P - actual)) && (j > 0))
+        {
+            j = j - 1;
         }
-        if(j==0 && C[j]>(P-actual)){
-            std::cout<<"No existe solucion"<<std::endl;
-            break;// Agregamos un break para salir del bucle si no hay solución
+        if (j == 0 && C[j] > (P - actual))
+        {
+            std::cout << "No existe solucion" << std::endl;
+            break; // Agregamos un break para salir del bucle si no hay solución
         }
-        X[j] = (P-actual)/(C[j]); // Agregamos una conversión explícita a double
-        actual = actual + C[j]*X[j]; // insertar
+        X[j] = (P - actual) / (C[j]);  // Agregamos una conversión explícita a double
+        actual = actual + C[j] * X[j]; // insertar
     }
 }
 
@@ -46,21 +50,40 @@ vector<int> cambioMonedas(int cantidad, const vector<int>& monedas) {
 }
 */
 
-
-
-int main() {
+void ejemplo1()
+{
     int cantidad = 389;
-    vector<int> monedas = {2,5,10,20,50,100,200}; // Diapositivas de la clase
+    vector<int> monedas = {2, 5, 10, 20, 50, 100, 200}; // Diapositivas de la clase
     vector<int> solucion(monedas.size());
     devolverCambio(cantidad, monedas, solucion);
 
-    cout << "Cambio: " << endl;
-    cout << "valor  \t=> cantidad\n";
-    for (int i = 0; i < monedas.size(); i++) {
-        cout << monedas[i] << " \t=> " << solucion[i] << endl;
-    }
+    cout << "Cambio: " << cantidad << endl;
+    cout << "valor  \t| cantidad\n";
 
-    vector<int> monedas2 = {100,90,1};
-    vector<int> solucion(monedas2.size());
+    for (int i = 0; i < monedas.size(); i++)
+        cout << monedas[i] << " \t| " << solucion[i] << endl;
+}
+
+void ejemplo2()
+{
+    int cantidad = 180;
+    vector<int> monedas = {100, 90, 1};
+    vector<int> solucion(monedas.size());
+    devolverCambio(cantidad, monedas, solucion);
+
+    cout << "Cambio: " << cantidad << endl;
+    cout << "valor  \t| cantidad\n";
+    for (int i = 0; i < monedas.size(); i++)
+        cout << monedas[i] << " \t| " << solucion[i] << endl;
+}
+
+int main()
+{
+    cout << "Ejemplo 1" << endl;
+    ejemplo1();
+
+    cout << "\nEjemplo 2" << endl;
+    ejemplo2();
+
     return 0;
 }
